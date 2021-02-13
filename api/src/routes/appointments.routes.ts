5d8cd302da18496151e951bf2ed65d0f84dba5fd
Lucas Neves <lucas54neves@outlook.com>
2021-02-13 19:37:19 -0300

    add appointment type and date validation

--- api/src/routes/appointments.routes.ts
+++ api/src/routes/appointments.routes.ts
@@ -1,26 +1,45 @@
-import { Router } from 'express'
-import { uuid } from 'uuidv4'
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
-const appointmentsRouter = Router()
+type Appointment = {
+  id: string;
+  provider: string;
+  date: Date;
+};
 
-const appointments = []
+const appointmentsRouter = Router();
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.get('/', (request, response) => {
-  return response.json(appointments)
-})
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body
+  const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked.' });
   }
 
-  appointments.push(appointment)
+  const appointment: Appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
 
-  return response.json(appointment)
-})
+  appointments.push(appointment);
 
-export default appointmentsRouter
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
