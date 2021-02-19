2de8b5a9ecc8bcda6e499528ab752800804f915d
Lucas Neves <lucas54neves@outlook.com>
2021-02-19 16:11:29 -0300

    formatting files

--- api/src/routes/appointments.routes.ts
+++ api/src/routes/appointments.routes.ts
@@ -1,36 +1,36 @@
-import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { Router } from 'express'
+import { uuid } from 'uuidv4'
+import { startOfHour, parseISO, isEqual } from 'date-fns'
+import Appointment from '../models/Appointment'
 
-const appointmentsRouter = Router();
+const appointmentsRouter = Router()
 
-const appointments: Appointment[] = [];
+const appointments: Appointment[] = []
 
 appointmentsRouter.get('/', (request, response) => {
-  return response.json(appointments);
-});
+  return response.json(appointments)
+})
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  const { provider, date } = request.body
 
-  const parsedDate = startOfHour(parseISO(date));
+  const parsedDate = startOfHour(parseISO(date))
 
   const findAppointmentInSameDate = appointments.find(appointment =>
     isEqual(parsedDate, appointment.date),
-  );
+  )
 
   if (findAppointmentInSameDate) {
     return response
       .status(400)
-      .json({ message: 'This appointment is already booked.' });
+      .json({ message: 'This appointment is already booked.' })
   }
 
-  const appointment = new Appointment(provider, parsedDate);
+  const appointment = new Appointment(provider, parsedDate)
 
-  appointments.push(appointment);
+  appointments.push(appointment)
 
-  return response.json(appointment);
-});
+  return response.json(appointment)
+})
 
-export default appointmentsRouter;
+export default appointmentsRouter
