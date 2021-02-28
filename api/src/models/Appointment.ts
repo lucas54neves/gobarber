0c1614fadd9492381c80bb6775bb213fdefd6fd2
Lucas Neves <lucas54neves@outlook.com>
2021-02-28 17:30:56 -0300

    fix appointments creation

--- api/src/models/Appointment.ts
+++ api/src/models/Appointment.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 
-@Entity('appoitments')
+@Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string
