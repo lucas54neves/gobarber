43a664c68aea5fd08c75b50d583c7f7f56b42d92
Lucas Neves <lucas54neves@outlook.com>
2021-03-15 14:12:57 -0300

    rename provider to provider_id atribute in services that creates appointments

--- api/src/services/CreateAppointmentService.ts
+++ api/src/services/CreateAppointmentService.ts
@@ -5,18 +5,18 @@ import Appointment from '../models/Appointment'
 import AppointmentsRepository from '../repositories/AppointmentsRepository'
 
 type Request = {
-  provider: string
+  provider_id: string
   date: Date
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository)
 
     const appointmentDate = startOfHour(date)
 
     const findAppointmentInSameDate = await appointmentsRepository.findByDate(
-      appointmentDate,
+      appointmentDate
     )
 
     if (findAppointmentInSameDate) {
@@ -24,8 +24,8 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
-      date: appointmentDate,
+      provider_id,
+      date: appointmentDate
     })
 
     await appointmentsRepository.save(appointment)
