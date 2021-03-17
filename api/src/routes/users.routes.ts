2b52baddaaa1cc7c67d8beaba54afbdd0cc60efe
Lucas Neves <lucas54neves@outlook.com>
2021-03-17 20:05:25 -0300

    add upload avatar route

--- api/src/routes/users.routes.ts
+++ api/src/routes/users.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
 import CreateUserService from '../services/CreateUserService'
+import multer from 'multer'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import uploadConfig from '../config/upload'
 
 type UserDTO = {
   name: string
@@ -8,6 +11,8 @@ type UserDTO = {
 }
 
 const usersRouter = Router()
+
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -28,5 +33,16 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message })
   }
 })
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file)
+
+    return response.json({ ok: true })
+  }
+)
 
 export default usersRouter
