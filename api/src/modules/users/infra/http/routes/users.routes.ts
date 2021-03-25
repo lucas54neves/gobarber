d13768f0bf02210d7e4bdcc02b23d9a0b24c81be
Lucas Neves <lucas54neves@outlook.com>
2021-03-25 19:46:15 -0300

    update users routes

--- api/src/modules/users/infra/http/routes/users.routes.ts
+++ api/src/modules/users/infra/http/routes/users.routes.ts
@@ -5,19 +5,21 @@ import CreateUserService from '@modules/users/services/CreateUserService'
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import uploadConfig from '@config/upload'
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository'
 
 type UserWithPasswordOptional = {
   password?: string
 }
 
 const usersRouter = Router()
-
 const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request: Request, response: Response) => {
   const { name, email, password } = request.body
 
-  const createUser = new CreateUserService()
+  const usersRepository = new UsersRepository()
+
+  const createUser = new CreateUserService(usersRepository)
 
   const user: UserWithPasswordOptional = await createUser.execute({
     name,
@@ -35,7 +37,9 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request: Request, response: Response) => {
-    const updateUserAvatar = new UpdateUserAvatarService()
+    const usersRepository = new UsersRepository()
+
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository)
 
     const user: UserWithPasswordOptional = await updateUserAvatar.execute({
       userId: request.user.id,
