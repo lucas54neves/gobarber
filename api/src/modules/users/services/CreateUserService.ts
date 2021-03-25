b3967b52048b91fcdc125e5c57a8f782640e40ef
Lucas Neves <lucas54neves@outlook.com>
2021-03-25 19:48:10 -0300

    update service that creates the users

--- api/src/modules/users/services/CreateUserService.ts
+++ api/src/modules/users/services/CreateUserService.ts
@@ -1,8 +1,9 @@
 import { getRepository } from 'typeorm'
 import { hash } from 'bcryptjs'
 
-import User from '@modules/users/infra/typeorm/entities/Users'
 import AppError from '@shared/errors/AppError'
+import IUserRepository from '@modules/users/repositories/IUsersRepository'
+import User from '@modules/users/infra/typeorm/entities/User'
 
 type Request = {
   name: string
@@ -11,12 +12,10 @@ type Request = {
 }
 
 class CreateUserService {
-  public async execute({ name, email, password }: Request): Promise<User> {
-    const usersRepository = getRepository(User)
+  constructor(private usersRepository: IUserRepository) {}
 
-    const checkUserExists = await usersRepository.findOne({
-      where: { email }
-    })
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email)
 
     if (checkUserExists) {
       throw new AppError('Email address already used.', 400)
@@ -24,13 +23,11 @@ class CreateUserService {
 
     const passwordHashed = await hash(password, 8)
 
-    const user = usersRepository.create({
+    const user = this.usersRepository.create({
       name,
       email,
       password: passwordHashed
     })
-
-    await usersRepository.save(user)
 
     return user
   }
