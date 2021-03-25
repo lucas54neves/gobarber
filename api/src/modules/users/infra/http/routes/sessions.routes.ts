a4af10b1e008d1cda23491bc95089ccf1296fdd9
Lucas Neves <lucas54neves@outlook.com>
2021-03-25 19:46:01 -0300

    update sessions routes

--- api/src/modules/users/infra/http/routes/sessions.routes.ts
+++ api/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,12 +1,14 @@
 import { Request, Response, Router } from 'express'
 import CreateSessionService from '@modules/users/services/CreateSessionService'
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository'
 
 const sessionsRouter = Router()
+const usersRepository = new UsersRepository()
 
 sessionsRouter.post('/', async (request: Request, response: Response) => {
   const { email, password } = request.body
 
-  const authenticatedUser = new CreateSessionService()
+  const authenticatedUser = new CreateSessionService(usersRepository)
 
   const { user, token } = await authenticatedUser.execute({
     email,
